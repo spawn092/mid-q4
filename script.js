282ae238a415be3f42f0787827fb23d78c6b13b5
cis101 <cis101@secs.iub.edu.bd>
2019-06-19 09:39:36 +0600

    Exam

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* Add your script here */
+function changeBGColor(event){
+    //console.log(event.target.innerText)
+    var body = document.getElementsByTagName('body')[0]
+    body.style.backgroundColor = event.target.innerText.toLowerCase()
+}
